3ce91ed22f7e9d3c6ed1a64f4fa7cedb25f9e46f
Cheng-Yang, Li <hsakira.k861@mmh.org.tw>
2024-03-19 16:36:44 +0800

    Workspace setup.

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{svelte,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui')
+  ],
+  daisyui:{
+    themes: ["retro", "wireframe"],
+    darkTheme: "dim",
+    base: true,
+    styled: true,
+    utils: true,
+    prefix:"",
+    logs: true,
+    themeRoot: ":root",
+  }
+}
+
