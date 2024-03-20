e16186d3fc89d19b953f3c6b3b924d88343398aa
Cheng-Yang, Li <hsakira.k861@mmh.org.tw>
2024-03-20 08:38:43 +0800

    Invoke @vercel/speed-insights.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import './styles/main.scss'
 import App from './App.svelte'
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
 const app = new App({
   target: document.getElementById('app')!,
 })
+injectSpeedInsights();
+export default app;
 
-export default app
+
