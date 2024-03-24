8bf75a1bff6c14fb213958610bc5840b4df53644
airewatterson <hsakira.k861@mmh.org.tw>
2024-03-25 00:08:06 +0800

    Update gravatar, and other unnecessary things.

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,28 @@
-<button>this is a test button</button>
+<style lang="scss">
+  body{
+    @apply flex flex-auto items-center justify-center h-screen text-center
+  }
+  h1 {
+    @apply font-bold
+  }
+</style>
+<script lang="ts">
+
+</script>
+
+<body>
+    <div>
+        <div class="avatar">
+            <div class="w-32 rounded-full shadow-md">
+                <img src="https://www.gravatar.com/avatar/68ff3955f6a62528adfc59a6f123998920d65ef832d4ef69050608f040091c54" alt="Moi.">
+            </div>
+            <div>
+                <i class="devicon-devicon-plain colored"></i>
+            </div>
+
+        </div>
+        <div class="flex flex-auto">
+
+        </div>
+    </div>
+</body>
